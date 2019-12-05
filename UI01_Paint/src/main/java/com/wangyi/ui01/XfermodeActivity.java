e99b456373f98964706d2330674fdf1f84ca6b48
joshuachang <joshachang0823@gmail.com>
2019-12-05 16:04:55 +0800

    1.Paint shader colorFilter 2.Xfermode 刮刮乐

---
+++ UI01_Paint/src/main/java/com/wangyi/ui01/XfermodeActivity.java
@@ -0,0 +1,14 @@
+package com.wangyi.ui01;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class XfermodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new XfermodeEraserView(this));
+    }
+}
